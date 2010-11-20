84df16d7a8211b0497383c5f0874c9e57b0c3140
Eric Druid <edruid@kth.se>
2010-11-20 14:19:28 +0100

    Adding interface to create settings.

---
+++ controllers/Setting.php
@@ -0,0 +1,62 @@
+<?php
+class SettingC extends Controller {
+	public function index($params) {
+		$this->_access_type('html');
+		global $session;
+		$this->_register('settings', Setting::selection(array(
+			'@order' => 'name',
+		)));
+		$this->_display('index');
+		new LayoutC('html');
+	}
+
+	public function edit($params) {
+		$this->_access_type('html');
+		global $session;
+		if($session == null || !$session->User->has_access('setting_editor')) {
+			Message::add_error("Du har inte access att skapa nya inställningar");
+			URL::redirect('');
+		}
+		$this->_display('edit');
+		new LayoutC('html');
+	}
+
+	public function create($params) {
+		$this->_access_type('html');
+		global $session;
+		if($session == null || !$session->User->has_access('setting_editor')) {
+			Message::add_error("Du har inte access att skapa nya inställningar");
+			URL::redirect('');
+		}
+		$this->_display('create');
+		new LayoutC('html');
+	}
+
+	public function make($params) {
+		$this->_access_type('script');
+		global $session;
+		if($session == null || !$session->User->has_access('setting_editor')) {
+			Message::add_error("Du har inte access att skapa nya inställningar");
+			URL::redirect('');
+		}
+		try{
+			$setting = new Setting();
+			$setting->name = ClientData::post('name');
+			$setting->code_name = ClientData::post('code_name');
+			$setting->commit();
+			URL::redirect('/Setting/index');
+		} catch(Exception $e) {
+			Message::add_error($e->getMessage());
+			URL::redirect('/Setting/Create');
+		}
+	}
+
+	public function modify($params) {
+		$this->_access_type('script');
+		global $session;
+		if($session == null || !$session->User->has_access('setting_editor')) {
+			Message::add_error("Du har inte access att skapa nya inställningar");
+			URL::redirect('');
+		}
+	}
+}
---
+++ views/Setting/create.php
@@ -0,0 +1,15 @@
+<form action="/Setting/make" method="post">
+	<fieldset>
+		<table>
+			<tr title="Namnet på inställningen som syns mot användarna">
+				<th>Namn</th>
+				<td><input type="text" name="name" /></td>
+			</tr>
+			<tr title="Namnet som används i koden för att kontrollera om rättigheten finns.">
+				<th>Kodnamn</th>
+				<td><input type="text" name="code_name" /></td>
+			</tr>
+		</table>
+		<input type="submit" value="Skapa ny inställning" />
+	</fieldset>
+</form>
