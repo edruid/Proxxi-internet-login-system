f4ff0e7174fa86397b8e1980bd61bdad7e24d2d9
Eric Druid <edruid@kth.se>
2010-11-21 21:59:28 +0100

    dividing css classes for Message types into message and type.

--- views/Message/index.php
+++ views/Message/index.php
@@ -1,10 +1,10 @@
 <? foreach(Message::get_errors() as $error): ?>
-	<p class="error"><?=$error?></p>
+	<p class="error message"><?=$error?></p>
 <? endforeach ?>
 <? foreach(Message::get_warnings() as $warning): ?>
-	<p class="warning"><?=$warning?></p>
+	<p class="warning message"><?=$warning?></p>
 <? endforeach ?>
 <? foreach(Message::get_notices() as $notice): ?>
-	<p class="notice"><?=$notice?></p>
+	<p class="message notice"><?=$notice?></p>
 <? endforeach ?>
 
