6ee26558f0d984ad60c465d5ca72957012c67d04
Eric Druid <edruid@kth.se>
2010-12-06 13:51:23 +0100

    Fixing layout of attendance create

--- views/Attendance/create.php
+++ views/Attendance/create.php
@@ -1,15 +1,18 @@
 <form action="/Attendance/make" method="post">
-	<table>
-		<tr>
-			<th>Användarnamn</th>
-			<td><input type="text" name="username" /></td>
-		</tr>
-		<tr>
-			<th>Dag</th>
-			<td><input type="text" name="day" value="<?=date('Y-m-d')?>"/></td>
-		</tr>
-		<tr>
-			<td colspan="2"><input type="submit" value="Rapportera" /></td>
-		</tr>
-	</table>
+	<fieldset>
+		<legend>Rapportera närvaro</legend>
+		<table>
+			<tr>
+				<th>Användarnamn</th>
+				<td><input type="text" name="username" /></td>
+			</tr>
+			<tr>
+				<th>Dag</th>
+				<td><input type="text" name="day" value="<?=date('Y-m-d')?>"/></td>
+			</tr>
+			<tr>
+				<td colspan="2"><input type="submit" value="Rapportera" /></td>
+			</tr>
+		</table>
+	</fieldset>
 </form>
